c4365594a0464f3f2a40e748ebc43e1923ddebef
sandip gawali <gawalisandip150@gmail.com>
2025-06-14 11:37:25 +0530

    SAUP-9 New Post (POST /api/posts/create) completed

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import pool from "./db.js";
 import bcrypt from "bcrypt";
-// import router from "./routes/post.js";
+import router from "./routes/post.js";
 
 dotenv.config();
 const app = express();
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-// app.use("/api/posts", router);
+app.use("/api/posts", router);
 
 // Register Route
 app.post("/api/register", async (req, res) => {
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,34 +1,34 @@
-// import express from "express";
-// import pool from "../db.js"; 
+import express from "express";
+import pool from "../db.js"; 
 
-// const router = express.Router();
+const router = express.Router();
  
 
-// // Create Post
-// router.post("/create", async (req, res) => {
-//   const { title, content, image } = req.body;
+// Create Post
+router.post("/create", async (req, res) => {
+  const { title, content, image } = req.body;
 
-//   try {
-//     const result = await pool.query(
-//       "INSERT INTO posts (title, content, image) VALUES ($1, $2, $3) RETURNING *",
-//       [title, content, image]
-//     );
-//     res.status(201).json({ message: "Post created", post: result.rows[0] });
-//   } catch (err) {
-//     console.error("Create post error:", err.message);
-//     res.status(500).json({ message: "Failed to create post" });
-//   }
-// });
+  try {
+    const result = await pool.query(
+      "INSERT INTO posts (title, content, image) VALUES ($1, $2, $3) RETURNING *",
+      [title, content, image]
+    );
+    res.status(201).json({ message: "Post created", post: result.rows[0] });
+  } catch (err) {
+    console.error("Create post error:", err.message);
+    res.status(500).json({ message: "Failed to create post" });
+  }
+});
 
-// // Get All Posts
-// router.get("/", async (req, res) => {
-//   try {
-//     const result = await pool.query("SELECT * FROM posts ORDER BY created_at DESC");
-//     res.status(200).json(result.rows);
-//   } catch (err) {
-//     console.error("Fetch posts error:", err.message);
-//     res.status(500).json({ message: "Failed to fetch posts" });
-//   }
-// });
+// Get All Posts
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM posts ORDER BY created_at DESC");
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error("Fetch posts error:", err.message);
+    res.status(500).json({ message: "Failed to fetch posts" });
+  }
+});
 
-// export default router;
+export default router;
